8a26b585c3df96bf8622cfe1586561cc2f20af2c
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-01-23 19:47:25 +0100

    Update main.py
    Added the snake game window closing.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame
-pygame.init()
-dis = pygame.display.set_mode((400,300))
-pygame.display.update()
+pygame.init() #Init of all imported pygame modules
+dis = pygame.display.set_mode((400,300)) #size of the snake game window
+pygame.display.update() #Updates the screen
+pygame.display.set_caption('GAME: Snake')
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # Closing the snake game
+            game_over = True
+
 pygame.quit()
 quit()
